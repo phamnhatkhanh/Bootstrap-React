3b40baed6989a723ae8f7da3cffdd3e4be56bd25
PhamNhatKhanh <khanhhcm4@gmail.com>
2021-06-29 21:02:20 +0700

    push code

---
+++ z_rut.js
@@ -0,0 +1,123 @@
+/* 
+  đọc -> hiểu web tổ chức ntn -- xem thử chỉnh sửa + thêm chỗ nào ko -- lăn tăn chỗ nào.
+HT: configure is same course ->  change and modify content
+NV: 
+1.read project -> visualization architecture ! not interested yet format css.
+2. modify code -> competele modun.
+3. add some cpo.
+dọc thêm css xem cách định dạng -> bk trinhg tới đâu
+
+header: focus navlink - form login: how they action + collasp.
+footer: Link vs NavLink, they action is same.
+*/
+
+/*
+  <NavLink className="nav-link" to='/aboutus'>: from file can jumb to place(file) different.
+*/
+
+
+
+
+
+
+
+
+
+
+
+/**
+*Head and Footer: know CPO and merg CPOs with together 
+! Just add bootstrap, App.css in App.js (file parent) is enough NOT only push tool for each component.
+
+*wk2 Reat Router:
+
+*wk2 Single Page Applications Part 1:
+ - Truyền tham số bên file main: hàm fitter.
+ - Home.js:
+    + RenderCard({item}): {item} nghĩa là gi đây.
+    + 
+
+*wk2 Single Page Applications Part 2:
+ - Changed handle event click(dishdetail) to File MENU from MAIN.
+   + Push data, Router in MAIN, Menu is same like store LINK.
+ - Use Rounter.
+
+*wk3 Control Form:
+ - Buit form use Reactbootstrap.
+ - Handel form( 2 function):
+  + fill data to Input.
+  + Click button.
+
+*wk3 Controlled Form Validation:
+ - Controlled: dùng hàm quản lý từng input luông trong nội tại Component.
+ ? Tạm thời sd nó Chưa tìm hiểu.
+ 
+ *wk3 Uncontrolled Form:
+ - Ref || innerRef: use DOM below (? but i don't how it work.
+#  Controlled Form or Uncontrolled Form: i see them create and not too diffenrt about GUI..
+
+*wk3 Intro Redux: giúp tổ chức các thành phần trong React chuyên biệt -> dễ buit hơn.
+ - Merg Redux and React.
+
+*wk3 React Redux Form Validation: xu ly logic $$ đỡ rườm rà hơn function.
+ - Modify 4 Row(f,l_name - contact - email).
+
+*wk4 Redux Actions: set submit messign -> comment ddc.
+ 1. return back assignment wk3: tao form 
+ 2. vao day them redux action.
+hhre
+
+*wk4 React Thux: action return lại 1 hàm thay cho oop
+hold 2 react này lại.
+*wk4 React Logger: hỗ trợ check lỗi tốt hơn DO truy vết lại đc luồng dữ liệu.
+---
+
+*wh4 Promise:hỗ trợ xly ACTION 
+ + True -> đưa function -> handel.
+ + False -> do something.
+
+ *wk4 Fetch: lq tới API -> tương tác với SERVER.
+  - Khi resquest || respone đẩy data cho Promise  xly.
+  - 
+
+
+*/
+
+/**
+572edc3 (HEAD) wk4 Fetch from Server: nhan data from SERVER and push to Project Example: in file Home
+1de6962 wk4 Combining Reducers
+6598846 wk3 React Redux Form Validation
+7cd9f13 wk3 Intro Redux: merg React and Redux.
+ad00c90 wk3 Uncontrolled Form
+b53d5ca wk3 Controlled Form Validation: pause hold BUT handle logic form not neccessary
+1b4855e wk3 Control Form
+a7d2736 wk2 Single .. App 2: change hand click Dishdetail and Router
+9f4bf2d Single Page Applications Part 1
+e50299b wk2 React Router
+3aa1529 wk2 Header and Footer P/s: use React not Bootstrap
+32d933b wk1 Assignment: DishDetail push INFO from MENU to DISH and know about "Cu phap"
+0e2fb0f (master) wk1 React Cpo_2: know use File DB import in App.js - handel and display DB in MenuComponent.js -- handel event click.
+2f2b1d7 wk1: React Component: buit and use define MY TAG,element in a MY TAG. 
+
+ */
+
+
+/**
+ *Truyền info sang file khác:
+ - <DishDetail dish={..} /> : name you can set evrything.
+ 
+ *State và Props:
+ - Props: bắt info mà file đưa vào
+ - State: Có Event muốn changed web -> set lại State.
+@ Constructor: muốn sd props || set tên gì cho state => intazilation.
+
+ *Cú Pháp:
+ - 
+
+ */
+
+/*???
+ ? cho class jumbotron sao khong hoat dong nho.
+ ? Trong div row, tách cpo chiếm bao nhiêu colum 
+   (? chèn code đoạn đó ~ nhiều khi lại bị lỗi.
+ */
